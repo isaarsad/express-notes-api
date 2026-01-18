74b2f22ee4b625e34d368e6ce3d60f26541d3c2e
isa-arsad <isaarsad0@gmail.com>
2026-01-18 12:18:00 +0700

    change port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import server from './server/index.js';
 
 const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log(`Server running at http://${host}:${port}`);
